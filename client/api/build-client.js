db54991a3591e423d49d2d99ea8a6fde1f613ebc
Yasas Kahawala <yasasravinda@hotmail.com>
2020-10-20 22:05:55 +0200

    testing 1

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
 		// We are on the server
 
 		return axios.create({
-			baseURL: 'http://www.testing-ticketingapp.xyz/',
+			baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
 			headers: req.headers
 		});
 	} else {
